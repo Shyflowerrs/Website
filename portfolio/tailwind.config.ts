6e8141e14698276b2085caaab3879ae9f7b261ac
chelseah-delphitech <chelseah@live.ca>
2024-05-29 15:56:26 -0500

    main header

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'mainBG': '#F1E5D1',
+        'bold': '#987070'
+      },
+
     },
   },
   plugins: [],
