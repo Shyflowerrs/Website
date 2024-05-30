4c3d4c2885607b849175dc8190a0541214c19e00
chelseah-delphitech <chelseah@live.ca>
2024-05-30 14:45:40 -0500

    Particle background working

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,8 +1,11 @@
+import Particle from "../components/Particles/Particles";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+    <Particle/>
     <main className="bg-mainBG">
       <p className="text-bold text-6xl text-center h-screen justify-center flex items-center">
         Hello, I'm Chelsea.<br/>
