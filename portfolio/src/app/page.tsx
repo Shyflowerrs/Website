49f523303568b4de71260e8010b6b61b469297be
chelseah-delphitech <chelseah@live.ca>
2024-06-13 15:21:00 -0500

    added new section for photo headshot

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -16,10 +16,12 @@ export default function Home() {
 
     </header>
 
-    <div className="bg-mainBG " id="about">
+    <div className="bg-mainBG   lg:h-screen mx-auto" id="about">
       <h1 className="text-bold  text-2xl sm:text-5xl lg:text-6xl text-center">ABOUT ME</h1>
-    <div className="container mx-auto columns-2">
-  <p>
+
+      <img  className="float-right"src="/myself.jpg" width="300" height="300"></img>
+      <p className="container">
+
   What is Lorem Ipsum?
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
@@ -36,7 +38,7 @@ Where can I get some?
 There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
     </p>
 </div>
-    </div>
+
 
     </>
   );
